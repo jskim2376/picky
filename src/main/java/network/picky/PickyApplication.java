af900020d5e13d698e008311d2621d89fd8e4940
padomay1352 <padomay1352@gmail.com>
2023-11-04 14:13:50 +0900

    Setting: Spring 설치

---
+++ src/main/java/network/picky/PickyApplication.java
@@ -0,0 +1,13 @@
+package network.picky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/network/picky/PickyApplicationTests.java
@@ -0,0 +1,13 @@
+package network.picky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
